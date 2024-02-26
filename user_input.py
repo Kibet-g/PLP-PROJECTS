c506fc17d90144d6cd111927c37472ed27396827
KIBET KORIR <93614316+Kibet-g@users.noreply.github.com>
2024-02-26 12:51:46 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("Please enter your name: ")
+    age = int(input("Please enter your age: "))  # Convert age to an integer
+    location = input("Please enter your location: ")
+
+    print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+if __name__ == "__main__":
+    main()
